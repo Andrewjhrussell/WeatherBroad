704ccd8eb40183ebbb9bbff51b284d311c6b3608
Andrewjhrussell <andrewjhrussell@gmail.com>
2020-05-21 18:11:30 -0400

    update

--- weather.js
+++ weather.js
@@ -1,57 +1,69 @@
-var  localData = localStorage.getItem("weather")
+var localData = localStorage.getItem("weather")
 if (!localData) {
-    localData =[]
-}else{
+    localData = []
+} else {
     localData = JSON.parse(localData)
 }
 function getWeather(city) {
     $.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=d73520d80a488bd40dc0c091724b64fa", function (data) {
-        console.log(data)
+        $.get("http://api.openweathermap.org/data/2.5/uvi?appid=d73520d80a488bd40dc0c091724b64fa&lat="+data.coord.lat+"&lon="+data.coord.lon, function (uv) {
+
+            console.log(data)
+            var curr = $("#currentWeather")
+            curr.empty()
+            var newH2 = $("<h2>").text(data.name)
+            var date = moment.unix(data.dt).format("l");
+            newH2.append(" (" + date + ")")
+            
+            var icon = "https://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            newH2.append($("<img>").attr("src", icon));
+            var temp = $("<div>").text("Temperature: " + data.main.temp + "°F")
+            var humidity = $("<div>").text("Humidity: " + data.main.humidity + "%")
+            var wind = $("<div>").text("Wind Speed: " + data.wind.speed + "mph")
+            var uv = $("<div>").text("UV: "+ uv.value)
 
 
-        var icon =
-            "https://api.openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        $(".icon").attr("src", "icon");
-
-
-        var temp = data.main.temp;
-        var weather = data.weather[0].main;
-        var rain = data.h1;
-
-        $(".icon").attr("src", icon);
-        $(".weather").append(weather);
-        $(".temp").append(temp);
-
-    }
-    );
+            curr.append(newH2,temp, humidity, wind, uv)
+        }
+        );
+    })
 }
-function newCityClick(clickCity){
+
+    // some api call
+    var dayCard = $("<div class='card bg-primary'>").text("Test:" )
+    $("#Andys-forecast").append(dayCard)
+
+   
+function newCityClick(clickCity) {
     getWeather(clickCity)
 }
 
-function generateList (){
-    console.log(localData)
-    if(localData.length !== 0){
+function generateList() {
+
+    if (localData.length !== 0) {
         var Ul = $('<ul class="list-group">')
         for (let i = 0; i < localData.length; i++) {
-           var newLi = $('<li class="list-group-item">').text(localData[i])
-            newLi.click(function(){newCityClick(localData[i])})
+            var newLi = $('<li class="list-group-item citySelect">').text(localData[i])
+            newLi.click(function () { newCityClick(localData[i]) })
             Ul.prepend(newLi)
         }
         $("#previousSearch").empty()
         $("#previousSearch").append(Ul)
-        getWeather(localData[localData.length-1])
+        getWeather(localData[localData.length - 1])
     }
 }
 
-$("#citySearch").submit(function(event){
+$("#citySearch").submit(function (event) {
     event.preventDefault()
     var cityName = $("#cityName").val()
-    if(cityName==="") return
+    if (cityName === "") return
     console.log(cityName)
-   
+
     localData.push(cityName)
     localStorage.setItem("weather", JSON.stringify(localData))
     generateList()
 })
 generateList()
+
+
+
